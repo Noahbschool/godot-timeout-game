250e3bcb259d86533b2bbf60b3b9df9cdae3ed71
NoahROC <1219897@student.roc-nijmegen.nl>
2026-02-05 13:48:10 +0100

    player fixes

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,25 +1,48 @@
 extends CharacterBody2D
 
+@export var speed: float = 10.0
+@export var jump_power: float = 10.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var speed_multiplier: float = 30.0
+var jump_multiplier: float = -40.0
 
+@onready var animation_player = $Node2D/AnimationPlayer
+@onready var sprite = $Node2D/Sprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Jump
 	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_power * jump_multiplier
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movement
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed * speed_multiplier
+		sprite.flip_h = direction < 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
 
 	move_and_slide()
+
+	update_animations(direction)
+
+
+func update_animations(direction):
+	var new_anim = ""
+
+	if not is_on_floor():
+		if velocity.y < 0:
+			new_anim = "jump"
+		else:
+			new_anim = "fall"
+	elif direction != 0:
+		new_anim = "move"
+	else:
+		new_anim = "idle"
+
+	if animation_player.current_animation != new_anim:
+		animation_player.play(new_anim)
