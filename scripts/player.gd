b4156aa2ba2528742bbcdc7beec270715d6838bf
clonnix <noahbrouwers24@gmail.com>
2026-02-10 08:52:25 +0100

    fixed level 1

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,25 +5,15 @@ extends CharacterBody2D
 
 var speed_multiplier: float = 30.0
 var jump_multiplier: float = -40.0
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 var can_control: bool = true
->>>>>>> parent of a332ed9 (reverted changed)
-=======
->>>>>>> parent of c68f4dd (things gone)
 
 @onready var animation_player = $Node2D/AnimationPlayer
 @onready var sprite = $Node2D/Sprite2D
 
 func _physics_process(delta: float) -> void:
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	if not can_control: return
->>>>>>> parent of a332ed9 (reverted changed)
-=======
->>>>>>> parent of c68f4dd (things gone)
+	if not can_control:
+		return
+
 	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -41,7 +31,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
 
 	move_and_slide()
-
 	update_animations(direction)
 
 
@@ -59,22 +48,7 @@ func update_animations(direction):
 		new_anim = "idle"
 
 	if animation_player.current_animation != new_anim:
-<<<<<<< HEAD
-<<<<<<< HEAD
 		if new_anim == "move":
-			animation_player.play(new_anim, -1.0, 2.5) # 1.5 = 150% speed
+			animation_player.play(new_anim, -1.0, 2.5)
 		else:
 			animation_player.play(new_anim)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	
->>>>>>> parent of a332ed9 (reverted changed)
-=======
->>>>>>> parent of c68f4dd (things gone)
-=======
-		animation_player.play(new_anim)
->>>>>>> parent of 99a8f76 (Merge branch 'main' of https://github.com/Noahbschool/godot-timeout-game)
-=======
-		animation_player.play(new_anim)
->>>>>>> parent of 91ee055 (added obstacles)
