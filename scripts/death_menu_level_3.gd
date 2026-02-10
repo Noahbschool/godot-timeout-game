b7d30ef73ca4a21c1dda7633d02b6a8b281e7582
clonnix <noahbrouwers24@gmail.com>
2026-02-10 13:11:43 +0100

    working death screen and deathzone

--- scripts/death_menu_level_3.gd
+++ scripts/death_menu_level_3.gd
@@ -4,4 +4,4 @@ func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _on_restart_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/level_one.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/level_three.tscn")
