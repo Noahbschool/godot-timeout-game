2e133b980b39b736dd2e2047930ef7eac786238f
NoahROC <1219897@student.roc-nijmegen.nl>
2026-02-11 11:19:29 +0100

    fixed pause menu

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,25 +1,30 @@
-extends Node
+extends Control
 
-var remaining_time: float = 0.0
-var paused: bool = false
-var pause_bonus: float = 5.0
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+	
+func resume() -> void:
+	GameState.set_paused(false)
 
-func start_level_timer(seconds: float) -> void:
-	remaining_time = seconds
-	paused = false
+func pause() -> void:
+	GameState.set_paused(true)
+
+func _on_main_menu_pressed() -> void:
 	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
-func toggle_pause_with_bonus() -> void:
-	remaining_time += pause_bonus
-	paused = not paused
-	get_tree().paused = paused
 
-func set_paused(value: bool) -> void:
-	paused = value
-	get_tree().paused = paused
+func _on_resume_pressed() -> void:
+	resume()
 
-func tick(delta: float) -> bool:
-	if paused:
-		return false
-	remaining_time -= delta
-	return remaining_time <= 0.0
+var was_paused := false
+
+func _process(_delta: float) -> void:
+	if get_tree().paused != was_paused:
+		was_paused = get_tree().paused
+		if was_paused:
+			visible = true
+			$AnimationPlayer.play("blur")
+		else:
+			$AnimationPlayer.play_backwards("blur")
+			visible = false
